9b2521559498dbfcda77a46e2becaa79bcbfb45b
Torstein Bratberg (Kursus Lokale) <tdbratberg@gmail.com>
2019-01-02 14:40:20 +0100

    Add project files.

---
+++ PhotoSharingTest/RoutingTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Routing;
+using System.Web.Mvc;
+using PhotoSharingTests.Doubles;
+using PhotoSharingApplication;
+
+namespace PhotoSharingTest
+{
+    [TestClass]
+    public class RoutingTests
+    {
+        [TestMethod]
+        public void Test_Default_Route_ControllerOnly()
+        {
+            var context = new FakeHttpContextForRouting(requestUrl: "~/ControllerName");
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+            RouteData routeData = routes.GetRouteData(context);
+            Assert.IsNotNull(routeData);
+            Assert.AreEqual("ControllerName", routeData.Values["controller"]);
+            Assert.AreEqual("Index", routeData.Values["action"]);
+        }
+
+        [TestMethod]
+        public void Test_Photo_Route_With_PhotoID()
+        {
+            var context = new FakeHttpContextForRouting(requestUrl: "~/photo/2");
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+            RouteData routeData = routes.GetRouteData(context);
+            Assert.IsNotNull(routeData);
+            Assert.AreEqual("Photo", routeData.Values["controller"]);
+            Assert.AreEqual("Display", routeData.Values["action"]);
+            Assert.AreEqual("2", routeData.Values["id"]);
+        }
+
+        [TestMethod]
+        public void Test_Photo_Title_Route()
+        {
+            var context = new FakeHttpContextForRouting(requestUrl: "~/photo/title/my%20title");
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+            RouteData routeData = routes.GetRouteData(context);
+            Assert.IsNotNull(routeData);
+            Assert.AreEqual("photo", routeData.Values["controller"]);
+            Assert.AreEqual("DisplayByTitle", routeData.Values["action"]);
+            Assert.AreEqual("my%20title", routeData.Values["title"]);
+        }
+    }
+}
